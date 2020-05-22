c8599c988194501b7bc315c95f5b4f21210af527
Gabe N <nelsong1997@gmail.com>
2020-05-21 21:51:55 -0400

    Updates

---
+++ precache-manifest.47fd81d7ffe5848879a543c5f4bac8fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5d0a46597f7321bf72fdcbe1472815",
+    "url": "/rps-infinity/index.html"
+  },
+  {
+    "revision": "3d79106870fd02eeafc4",
+    "url": "/rps-infinity/static/css/main.5ef7a37f.chunk.css"
+  },
+  {
+    "revision": "212f78a41170394f4da9",
+    "url": "/rps-infinity/static/js/2.a8f54189.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rps-infinity/static/js/2.a8f54189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d79106870fd02eeafc4",
+    "url": "/rps-infinity/static/js/main.9b06e626.chunk.js"
+  },
+  {
+    "revision": "7eeabb931bdfcaff894e",
+    "url": "/rps-infinity/static/js/runtime-main.862ebe0d.js"
+  }
+]);
